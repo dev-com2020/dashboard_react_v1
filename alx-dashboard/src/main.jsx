b76e18e6fbe812c8c82c29b46373406bc52b09bc
Tomek <kwp@tlen.pl>
2026-03-13 14:54:47 +0100

    Zaktualizowano ścieżkę bazową w komponencie BrowserRouter na '/dashboard_react_v1/'

--- alx-dashboard/src/main.jsx
+++ alx-dashboard/src/main.jsx
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/alx-dashboard'>
+      <BrowserRouter basename='/dashboard_react_v1/'>
            <App />
       </BrowserRouter>
     </Provider>
